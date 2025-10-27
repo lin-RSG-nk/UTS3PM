3160770bc368b3806df07ae3f9d4eca016a329ca
Rifan Sahrul Gunawan <12450113215@students.uin-suska.ac.id>
2025-10-27 14:19:21 +0700

    Update BagianArrayList.java

--- UTS3PMM/src/Penampungan/BagianArrayList.java
+++ UTS3PMM/src/Penampungan/BagianArrayList.java
@@ -1,9 +1,8 @@
 package Penampungan;
 
 import Model.Mahasiswa;
-
 import java.util.ArrayList;
-
+import java.util.Comparator; 
 
 public class BagianArrayList {
     private ArrayList<Mahasiswa> list = new ArrayList<>();
@@ -40,15 +39,22 @@ public class BagianArrayList {
         return false;
     }
 
-
-    public void TampilkanData() {
+    public void TampilkanData(String criteria) { 
         if (list.isEmpty()) {
             System.out.println("Data masih kosong.");
             return;
         }
+
+        if (criteria.equals("IPK")) {
+            list.sort((m1, m2) -> Double.compare(m2.getIpk(), m1.getIpk()));
+        } else {
+            list.sort(Comparator.comparing(Mahasiswa::getNim));
+        }
+
         System.out.println("==============================================");
         System.out.printf("%-15s %-20s %-15s %-5s\n", "NIM", "NAMA", "PRODI", "IPK");
         System.out.println("==============================================");
+
         for (Mahasiswa mhs : list) {
             System.out.printf("%-15s %-20s %-15s %-5.2f\n",
                     mhs.getNim(), mhs.getNama(), mhs.getProdi(), mhs.getIpk());
